c2ce812d852c9ee69f402fb85192450b86d2df18
坏黑 <b@skymc.me>
2022-02-11 18:44:14 +0800

    Update

--- src/main/groovy/org/tabooproject/shrinkingkt/Plugin.groovy
+++ src/main/groovy/org/tabooproject/shrinkingkt/Plugin.groovy
@@ -130,13 +130,13 @@ class ShrinkingClassVisitor extends ClassVisitor {
         return super.visitMethod(access, name, descriptor, signature, exceptions)
     }
 
-    @Override
-    void visitSource(String source, String debug) {
-        if (exclude) {
-            return
-        }
-        super.visitSource(source, debug)
-    }
+//    @Override
+//    void visitSource(String source, String debug) {
+//        if (exclude) {
+//            return
+//        }
+//        super.visitSource(source, debug)
+//    }
 }
 
 class ShrinkingMethodVisitor extends MethodVisitor {
